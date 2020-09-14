a04ff9efb9f9e8d426c1eb8f5d004aed68337c63
Rob Lawrence <iamrobnyce@gmail.com>
2020-09-14 15:30:00 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,11 @@
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to you and me"
+
+#puts "Rob is going to figure it always"
+
+name = "Luca"
+ 
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in
+the store."
+
